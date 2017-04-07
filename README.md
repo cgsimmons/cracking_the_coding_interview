7437634ec2c77c18175c0e5f0773a26e84f074aa
cgsimmons <chris.simmons.alt@gmail.com>
2017-04-07 15:29:25 -0700

    Initialize repo

---
+++ README.md
@@ -0,0 +1,20 @@
+## Cracking the Coding Interview: Exercises in Ruby
+
+Using Ruby to solve exercises from the book.
+
+
+## INSTALLATION
+
+Simple install the bundle to get rspec.
+
+```bash
+bundle
+```
+
+## RSPEC
+
+To run rspec on a given script:
+
+```bash
+rspec chpt_01/1.1_unique_string.rb
+```
---
+++ chpt_01/1.1_unique_string.rb
@@ -0,0 +1,26 @@
+require'spec_helper'
+
+def dups?(s)
+  return false if s.size <= 1
+  s.split('').uniq.size != s.split('').size
+end
+
+describe 'dups?' do
+  it 'returns false for an empty or one-element string' do
+    expect(dups?('')).to be false
+    expect(dups?('a')).to be false
+  end
+  it 'returns false for a string with no dups' do
+    expect(dups?('12')).to be false
+    expect(dups?('123')).to be false
+  end
+  it 'returns true for a string with dups' do
+    expect(dups?('aa')).to be true
+    expect(dups?('abacdea')).to be true
+  end
+  it 'properly handles embedded newlines' do
+    expect(dups?('abc\n456')).to be false
+    expect(dups?('abc\n456c')).to be true
+    expect(dups?('abc\n456\n')).to be true
+  end
+end
